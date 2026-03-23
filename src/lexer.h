ed51ae5deceb9f8a4a03871c4b7c32c487e30fb0
WillEdgington <willedge037@gmail.com>
2026-03-23 18:15:48 +0000

    feat: define Token and Lexer types in lexer.h

--- src/lexer.h
+++ src/lexer.h
@@ -1,1 +1,36 @@
-/* placeholder */
+#ifndef LEXER_H
+#define LEXER_H
+
+#include <stddef.h>
+
+typedef enum {
+  LBRACE,
+  RBRACE,
+  LSQUAREBRACKET,
+  RSQUAREBRACKET,
+  COMMA,
+  COLON,
+  STRING,
+  NUMBER,
+  JSON_TRUE,
+  JSON_FALSE,
+  JSON_NULL,
+  TOKEN_EOF,
+} TokenType;
+
+typedef union {
+  char *string;
+  double number;
+} TokenValue;
+
+typedef struct {
+  TokenType type;
+  TokenValue value;
+} Token;
+
+typedef struct {
+  const char *src;
+  size_t pos;
+} Lexer;
+
+#endif
