91b91e2f45a769148476944833dcd528eae2f502
WillEdgington <willedge037@gmail.com>
2026-04-03 17:01:15 +0100

    feat: update main.c to support optional path arguement

--- src/main.c
+++ src/main.c
@@ -1,18 +1,19 @@
 #include "json.h"
 #include "parser.h"
+#include "query.h"
 #include "read.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
   if (argc < 2) {
-    printf("usage: cjson <file>\n");
+    printf("usage: cjson <file> [path]\n");
     return 1;
   }
 
   char *src = read_file(argv[1]);
   if (src == NULL) {
-    printf("error: could not read file\n");
+    printf("error: could not read file %s\n", argv[1]);
     return 1;
   }
 
@@ -20,13 +21,25 @@ int main(int argc, char **argv) {
   parser_init(&parser, src);
   JsonNode *root = parser_parse(&parser);
   free(src);
+
   if (root == NULL) {
     printf("parse error\n");
     return 1;
   }
 
-  json_print(root);
-  printf("\n");
+  if (argc >= 3) {
+    JsonNode *result = json_get_path(root, argv[2]);
+    if (result) {
+      json_print(result);
+      printf("\n");
+    } else {
+      printf("Path not found or invalid.\n");
+    }
+  } else {
+    json_print(root);
+    printf("\n");
+  }
+
   json_free(root);
   return 0;
 }
